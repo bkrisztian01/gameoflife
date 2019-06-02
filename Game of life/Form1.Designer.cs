d299a9c3d2e33bfd96890b3fd29b762c978a1849
bkrisztian01 <bui.krisztian01@gmail.com>
2019-06-02 18:13:19 +0200

    First commit

---
+++ Game of life/Form1.Designer.cs
@@ -0,0 +1,124 @@
+﻿namespace Game_of_life
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.button2 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(500, 500);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(332, 509);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Reset grid";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 500;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(413, 509);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Start";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(14, 514);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(64, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Interval (ms)";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(84, 511);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 4;
+            this.textBox1.Text = "500";
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(500, 541);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBox1);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.Text = "Game of Life";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
---
+++ Game of life/Form1.cs
@@ -0,0 +1,202 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows;
+
+namespace Game_of_life
+{
+    public partial class Form1 : Form
+    {
+        public bool[,] cells = new bool[50, 50];
+        public bool started = false;
+        Bitmap kép = new Bitmap(500, 500);
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        void uresNegyzet(int x, int y)
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                kép.SetPixel(x * 10 + i, y * 10, Color.LightGray);
+                kép.SetPixel(x * 10 + i, y * 10 + 9, Color.LightGray);
+                kép.SetPixel(x * 10, y * 10 + i, Color.LightGray);
+                kép.SetPixel(x * 10 + 9, y * 10 + i, Color.LightGray);
+            }
+        }
+
+        void teliNegyzet(int x, int y, Color szín)
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                for (int j = 0; j < 10; j++)
+                {
+                    kép.SetPixel(x * 10 + i, y * 10 + j, szín);
+                }
+            }
+        }
+        
+        public int checkNeighbours(int x, int y)
+        {
+            if (!(x < 0 || x > 49 || y < 0 || y > 49))
+            {
+                if (cells[x, y] == true)
+                {
+                    return 1;
+                }
+                else
+                {
+                    return 0;
+                }
+            }
+            return 0;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            pictureBox1.BackColor = Color.White;
+            DoubleBuffered = true;
+            pictureBox1.Image = kép;
+            for (int i = 0; i < 50; i++)
+            {
+                for (int j = 0; j < 50; j++)
+                {
+                    teliNegyzet(i, j, Color.White);
+                    uresNegyzet(i, j);
+                }
+            }
+            for (int i = 0; i < 50; i++)
+            {
+                for (int j = 0; j < 50; j++)
+                {
+                    cells[i, j] = false;
+                }
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            timer1.Stop();
+            button2.Text = "Start";
+            started = false;
+            for (int i = 0; i < 50; i++)
+            {
+                for (int j = 0; j < 50; j++)
+                {
+                    teliNegyzet(i, j, Color.White);
+                    uresNegyzet(i, j);
+                }
+            }
+            for (int i = 0; i < 50; i++)
+            {
+                for (int j = 0; j < 50; j++)
+                {
+                    cells[i, j] = false;
+                }
+            }
+            pictureBox1.Image = kép;
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            MouseEventArgs me = (MouseEventArgs)e;
+            Point coordinates = me.Location;
+            int x = (coordinates.X - (coordinates.X % 10)) / 10;
+            int y = (coordinates.Y - (coordinates.Y % 10)) / 10;
+            if (!cells[x, y])
+            {
+                cells[x, y] = true;
+                teliNegyzet(x, y, Color.Black);
+            }
+            else
+            {
+                cells[x, y] = false;
+                teliNegyzet(x, y, Color.White);
+                uresNegyzet(x, y);
+            }
+            pictureBox1.Image = kép;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (!started)
+            {
+                timer1.Start();
+                button2.Text = "Stop";
+            }
+            else if (started)
+            {
+                timer1.Stop();
+                button2.Text = "Start";
+            }
+            started = !started;
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            bool[,] futureCells = new bool[50, 50];
+            for (int i = 0; i < 50; i++)
+            {
+                for (int j = 0; j < 50; j++)
+                {
+                    futureCells[i, j] = false;
+                    int livingCellsNearby = 0;
+                    livingCellsNearby += checkNeighbours(i - 1, j - 1) + checkNeighbours(i - 1, j) + checkNeighbours(i - 1, j + 1) +    //XXX
+                        checkNeighbours(i, j - 1) + checkNeighbours(i, j + 1) +                                                         //XOX
+                        checkNeighbours(i + 1, j - 1) + checkNeighbours(i + 1, j) + checkNeighbours(i + 1, j + 1);                      //XXX
+                    if (cells[i,j] && (livingCellsNearby == 2 || livingCellsNearby == 3))
+                    {
+                        futureCells[i,j] = true;
+                    }
+                    else if (!cells[i, j] && livingCellsNearby == 3)
+                    {
+                        futureCells[i, j] = true;
+                    }
+
+                }
+            }
+            
+            for (int i = 0; i < 50; i++)
+            {
+                for (int j = 0; j < 50; j++)
+                {
+                    if (futureCells[i, j] != cells[i, j])
+                    {
+                        if (futureCells[i, j] == true)
+                        {
+                            teliNegyzet(i, j, Color.Black);
+                        }
+                        else if (futureCells[i, j] == false)
+                        {
+                            teliNegyzet(i, j, Color.White);
+                            uresNegyzet(i, j);
+                        }
+                    }
+                }
+            }
+
+            cells = futureCells;
+            pictureBox1.Image = kép;
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            if (textBox1.Text != "" && Convert.ToInt32(textBox1.Text) != 0)
+            {
+                timer1.Interval = Convert.ToInt32(textBox1.Text);
+            }
+            else if (textBox1.Text != "" && Convert.ToInt32(textBox1.Text) == 0)
+            {
+                MessageBox.Show("Nem lehet 0");
+            }
+        }
+    }
+}
